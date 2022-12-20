3ba56826ce7438e463a5fafcc4607e85e3d2d29a
DebugHyena <maciej.staniec@yahoo.co.uk>
2022-12-20 15:04:40 +0000

    commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+// Selectors
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
+const letsPlayBtn = document.querySelector(".play-btn");
+const selectBtns = document.querySelectorAll(".select-btn");
+const playerHand = document.querySelector("#player-hand");
+const compHand = document.querySelector("#comp-hand");
+
+// Listeners
+letsPlayBtn.addEventListener("click", startGame);
+//  ### Adding listeners to play buttons ###
+for (let i = 0; i < selectBtns.length; i++) {
+  selectBtns[i].addEventListener("click", drawOption);
+}
+
+// Functions
+function startGame() {}
+
+function drawOption() {}
